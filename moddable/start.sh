0b04d9cf833ad41b158b65edf63f8cd00581cb8f
Diogo Vianna <diogoviannaaraujo@gmail.com>
2020-06-08 20:40:31 -0300

    Fixing start script.

--- moddable/start.sh
+++ moddable/start.sh
@@ -2,12 +2,5 @@
 
 echo "STARTING XVFB"
 Xvfb :1 -screen 0 1280x720x24 &
-
-if [ -z ${VNC_PASS} ]; then
-  echo "STARTING VNC WITHOUT PASSWORD"
-  x11vnc -shared -display :1 -oa /var/log/x11vnc.log
-
-else
-  echo "STARTING VNC WITH PASSWORD"
-  x11vnc -shared -display :1 -passwd %(ENV_VNC_PASS)s -oa /var/log/x11vnc.log
-fi
+echo "STARTING X11VNC"
+x11vnc -shared -display :1 -oa /var/log/x11vnc.log
