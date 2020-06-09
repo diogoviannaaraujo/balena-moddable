fc6dd5d4f7ccd277e28b2f39bd0438c8efb2217b
Diogo Vianna <diogoviannaaraujo@gmail.com>
2020-06-09 19:54:21 -0300

    Update start script.c

--- moddable/start.sh
+++ moddable/start.sh
@@ -3,7 +3,7 @@
 echo "STARTING XVFB"
 Xvfb :1 -screen 0 1280x720x24 &
 echo "STARTING X11VNC"
-x11vnc -shared -display :1 -oa /var/log/x11vnc.log
+x11vnc -shared -display :1 -oa /var/log/x11vnc.log &
 sleep 30
 echo "STARTING LXDE"
 DISPLAY=:1 startlxde
