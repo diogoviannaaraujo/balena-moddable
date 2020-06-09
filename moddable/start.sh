df4b561b409311d4ac9460dbe6813c18b596cbc2
Diogo Vianna <diogoviannaaraujo@gmail.com>
2020-06-09 02:35:04 -0300

    Added terminal dependecy

--- moddable/start.sh
+++ moddable/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+DISPLAY=:1
+
 echo "STARTING XVFB"
 Xvfb :1 -screen 0 1280x720x24 &
 echo "STARTING X11VNC"
-x11vnc -shared -display :1 -oa /var/log/x11vnc.log
+x11vnc -shared -display :1 -oa /var/log/x11vnc.log &
+echo "STARTING GNOME TERMINAL"
+gnome-terminal
