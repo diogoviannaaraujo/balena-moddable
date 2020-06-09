9d830a786972f2876f1fb8efa8a6609bea9b02e6
Diogo Vianna <diogoviannaaraujo@gmail.com>
2020-06-09 16:47:51 -0300

    Update start script.c

--- moddable/start.sh
+++ moddable/start.sh
@@ -4,5 +4,6 @@ echo "STARTING XVFB"
 Xvfb :1 -screen 0 1280x720x24 &
 echo "STARTING X11VNC"
 x11vnc -shared -display :1 -oa /var/log/x11vnc.log
-#echo "STARTING GNOME TERMINAL"
-#DISPLAY=:1 gnome-terminal
+sleep 30
+echo "STARTING LXDE"
+DISPLAY=:1 startlxde
