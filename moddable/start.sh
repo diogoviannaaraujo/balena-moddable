ccd3eeb91ea367c91e24ec504cab5220f4991608
Diogo Vianna <diogoviannaaraujo@gmail.com>
2020-06-09 11:51:10 -0300

    Update start script.

--- moddable/start.sh
+++ moddable/start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-DISPLAY=:1
-
 echo "STARTING XVFB"
 Xvfb :1 -screen 0 1280x720x24 &
 echo "STARTING X11VNC"
 x11vnc -shared -display :1 -oa /var/log/x11vnc.log &
 echo "STARTING GNOME TERMINAL"
-gnome-terminal
+DISPLAY=:1 gnome-terminal
