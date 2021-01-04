e7bf433d2c4cbfa9fdc98799a0bf2a13ac224665
Diogo Vianna V. Araújo <diogoviannaaraujo@Diogos-MacBook-Air.local>
2021-01-04 15:10:56 -0300

    start script only sleeps.

--- start.sh
+++ start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-echo "STARTING XVFB"
-Xvfb :1 -screen 0 1280x720x24 &
-echo "STARTING X11VNC"
-x11vnc -shared -display :1 -oa /var/log/x11vnc.log &
-sleep 30
-echo "STARTING LXDE"
-DISPLAY=:1 startlxde
+sleep 86400
 echo "DONE"
